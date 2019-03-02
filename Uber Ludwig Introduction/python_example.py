d880d93b8de86ed6e6f0278684e46a8c2cdb167c
Gilbert Tanner <gilberttanner.work@gmail.com>
2019-03-02 17:05:44 +0100

    uber ludwig introduction

---
+++ Uber Ludwig Introduction/python_example.py
@@ -0,0 +1,20 @@
+from ludwig import LudwigModel
+import pandas as pd
+
+df = pd.read_csv('Tweets.csv')[['airline_sentiment', 'content']]
+print(df.head())
+
+model_definition = {
+    'input_features':[
+        {'name':'content', 'type':'text'},
+    ],
+    'output_features': [
+        {'name': 'airline_sentiment', 'type': 'category'}
+    ]
+}
+
+print('creating model')
+model = LudwigModel(model_definition)
+print('training model')
+train_stats = model.train(data_df=df)
+model.close()
